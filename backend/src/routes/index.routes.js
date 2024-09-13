f8ab705d38eb1b98356050c1ff9dfb1d566b40fc
unknown <pablo.castillo2101@alumnos.ubiobio.cl>
2024-09-13 17:19:10 -0300

    add ruta archivo en index.routes.js

--- backend/src/routes/index.routes.js
+++ backend/src/routes/index.routes.js
@@ -2,11 +2,13 @@
 import { Router } from "express";
 import userRoutes from "./user.routes.js";
 import authRoutes from "./auth.routes.js";
+import archivoRoutes from "./archivo.routes.js";
 
 const router = Router();
 
 router
     .use("/auth", authRoutes)
+    .use("/archivo", archivoRoutes)
     .use("/user", userRoutes);
 
 export default router;
